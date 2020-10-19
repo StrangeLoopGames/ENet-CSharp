5707ac78886626c4a9091955f08b5698c947b01f
alexander.bondarev <alex@strangeloopgames.com>
2020-10-19 14:30:28 +0300

    1. Extracted AppVeyor runtimes download to function. 2. Added BuildUtils-BuildPackage PS function which prepares and packs nuget package and returns it's name. 3. Added PushToLocal.ps1 which builds package and then pushes it to $HOME/.nuget-local source. It also removes cached package installation.

--- Scripts/UpdateRuntimesFromAppVeyor.ps1
+++ Scripts/UpdateRuntimesFromAppVeyor.ps1
@@ -2,72 +2,12 @@ param([string]$Token = "", [string]$AccountName = "mirasrael", [string]$BuildId 
 
 if ($Help)
 {
-    Write-Host "USAGE: powershell ./UpdateRuntimeFromAppVeyor.ps1 [-Token <Token>] [-AccountName <AccountName>] [-Help]"
+    Write-Host "USAGE: powershell ./UpdateRuntimeFromAppVeyor.ps1 [-Token <Token>] [-AccountName <AccountName>] [-BuildId <BuildId>] [-Help]"
     Exit 0
 }
 
-$apiUrl = 'https://ci.appveyor.com/api'
+$ErrorActionPreference = "Stop"
 
-if ($Token -eq "")
-{
-    $tokenFilePath = "$HOME/.appveyor/token"
-    if ([System.IO.File]::Exists($tokenFilePath))
-    {
-        $Token = Get-Content -Path $tokenFilePath
-    }
-    else
-    {
-        Write-Host "AppVeyor token not found";
-        Exit 1;
-    }
-}
+. $PSScriptRoot/BuildUtils.ps1
 
-$token = $Token
-$headers = @{
-    "Authorization" = "Bearer $token"
-    "Content-type" = "application/json"
-}
-
-$accountName = $AccountName
-$projectSlug = 'enet-csharp'
-
-$runtimesPath = '$PSScriptRoot\..\runtimes'
-
-if ($BuildId -eq "")
-{
-    # get project with last build details
-    $project = Invoke-RestMethod -Method Get -Uri "$apiUrl/projects/$accountName/$projectSlug" -Headers $headers
-}
-else
-{
-    # get project with specific build details
-    $project = Invoke-RestMethod -Method Get -Uri "$apiUrl/projects/$accountName/$projectSlug/build/$BuildId" -Headers $headers
-}
-
-# download artifacts for all jobs in the build
-foreach ($job in $project.build.jobs)
-{
-    $jobId = $job.jobId
-
-    # get job artifacts (just to see what we've got)
-    $artifacts = Invoke-RestMethod -Method Get -Uri "$apiUrl/buildjobs/$jobId/artifacts" -Headers $headers
-
-    foreach ($artifact in $artifacts)
-    {
-        $artifactFileName = $artifact.fileName
-        $libName = Split-Path -Path $artifactFileName -Leaf
-        $runtime = switch ($job.name)
-        {
-            "Image: Visual Studio 2019; Platform: x86" { "win-x86" }
-            "Image: Visual Studio 2019; Platform: x64" { "win-x64" }
-            "Image: Ubuntu; Platform: x64" { "linux-x64" }
-            "Image: macOS; Platform: x64" { "osx-x64" }
-        }
-        $artifactUri = "$apiUrl/buildjobs/$jobId/artifacts/$artifactFileName"
-        $localArtifactDir = "$runtimesPath/$runtime/native"
-        [void](New-Item -ItemType Directory -Force -Path $localArtifactDir)
-        $localArtifactPath = "$localArtifactDir/$libName"
-        Write-Host "$artifactUri -> $localArtifactPath"
-        Invoke-RestMethod -Method Get -Uri "$apiUrl/buildjobs/$jobId/artifacts/$artifactFileName" -OutFile $localArtifactPath -Headers $headers
-    }
-}
+BuildUitls-DownloadAppVeyorRuntimes -Token Token -AccountName $AccountName -BuildId $BuildId
